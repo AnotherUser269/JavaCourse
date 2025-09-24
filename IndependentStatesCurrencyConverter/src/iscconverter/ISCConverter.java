0e8f1f33450151156cad2666b99c6599d9e9f02e
AnotherUser269 <studyman269@gmail.com>
2025-09-25 00:21:28 +0300

    Initial Commit

---
+++ IndependentStatesCurrencyConverter/src/iscconverter/ISCConverter.java
@@ -0,0 +1,62 @@
+package iscconverter;
+import java.util.*;
+
+public class ISCConverter {
+    public static void main(String[] args) {
+        double RUB = 1.0;    // Рубли
+        double KZT = 0.1536; // Казахстан
+        double BYN = 0.0364; // Беларусь
+        double UAH = 0.4964; // Украина
+
+        // Filling the HashMap
+        Map<String, Double> currencies = new HashMap<String, Double>();
+        currencies.put("RUB", RUB);
+        currencies.put("KZT", KZT);
+        currencies.put("BYN", BYN);
+        currencies.put("UAH", UAH);
+        
+        // Logic
+        System.out.println("Welcome to the currency converter!");
+        System.out.println("Everything is correct on 24.09.2025.\n");
+
+        System.out.println("Please choose 2 currencies from the following list:\n");
+        System.out.println("1) RUB\n2) KZT\n3) BYN\n4) UAH\n");
+
+        System.out.println("And write a special notation: amount CURRENCY1 -> CURRENCY2\n");
+
+        System.out.println("\n> Your input: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        String[] data = userInput.split(" ");
+
+        if (data.length != 4 || !(data[2].equals("->"))) {
+            System.out.println("\nYou didn't followed the notation.");
+        }
+        else {
+            try {
+                double amount = Double.valueOf(data[0]);
+
+                String cur1 = data[1].toUpperCase();
+                String cur2 = data[3].toUpperCase();
+
+                if(!(currencies.containsKey(cur1)) || !(currencies.containsKey(cur2))) {
+                    System.out.println("\nYou have provided a wrong currency!");
+                }
+                else {
+                    double convertedAmount = amount*currencies.get(cur1)/currencies.get(cur2);
+
+                    System.out.println("\n" + amount + " " + cur1 + " = " + convertedAmount + " " + cur2);
+                }
+            }
+            catch (NumberFormatException ex){
+                System.out.println("\nYou have provided a wrong amount!");
+            }
+            catch (Exception e) {
+                System.out.println("\nSomething went wrong.");
+            }
+        }   
+
+    }
+}
